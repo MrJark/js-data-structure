5488b98ba22cbb3aeea020bac16fd792718a3051
MrJark <mrjark7@gmail.com>
2023-02-20 06:16:04 +0100

    another challenge that i couldn't complete it. IT'S VERY FRUSTRATING

--- hash/hasttable.js
+++ hash/hasttable.js
@@ -42,5 +42,23 @@ class HastTable {
         }
         return undefined;
     }
+    getAllKeys(){// reto que no he podido completar
+        let keys = []
+        for (let i = 0; i < this.data.length; i++) {
+          const element = this.data[i]
+          if(element){
+            if(element.length > 1){
+              for (let j = 0; j < element.length; j++) {
+                const item = element[j];
+                keys = [...keys, item[0]]
+              }
+            }else{
+              keys = [...keys, element[0][0]]
+            }
+          }
+        }
+    
+        return keys
+    }
 }
 const myHashTable = new HashTable(50);
